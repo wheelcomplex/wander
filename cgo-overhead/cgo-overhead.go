8825d71f5ee0db5323bdcca1caeb064edc4b4fc7
YinJiaJin <yinjiajin@xiaomi.com>
2015-12-28 14:33:19 +0800

    re-format cgo-overhead test

--- cgo-overhead/cgo-overhead.go
+++ cgo-overhead/cgo-overhead.go
@@ -27,18 +27,19 @@ var calls = flag.Int("n", 1e8, "number of func calls")
 
 func main() {
 	flag.Parse()
+	fmt.Printf("CGO vs NATIVE, %d calls testing ...\n", *calls)
 	now := time.Now()
 	for i := 0; i < *calls; i++ {
 		C.add(C.int(i))
 	}
 	c_dur_time := time.Now().Sub(now)
-	fmt.Printf("CGO %d calls: CGO %v\n", *calls, c_dur_time.String())
+	fmt.Printf("CGO : %v\n", c_dur_time)
 	now = time.Now()
 	for i := 0; i < *calls; i++ {
 		add(i)
 	}
 	g_dur_time := time.Now().Sub(now)
-	fmt.Printf("NATIVE %d calls: NATIVE %v\n", *calls, g_dur_time.String())
-	fmt.Printf("CGO %d calls: CGO %v - NATIVE %v = %v, CGO / NATIVE = %v \n", *calls, c_dur_time, g_dur_time,
-		(c_dur_time - g_dur_time).String(), int64(c_dur_time/g_dur_time))
+	fmt.Printf("NATIVE: %v\n", g_dur_time)
+	fmt.Printf("DIFF: CGO %v - NATIVE %v = %v, CGO / NATIVE = %v \n",
+		c_dur_time, g_dur_time, (c_dur_time - g_dur_time), int64(c_dur_time/g_dur_time))
 }
