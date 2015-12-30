ad21abd1316278685f51bbad5859710470dfd626
YinJiaJin <yinjiajin@xiaomi.com>
2015-12-30 11:59:55 +0800

    optimated lixuancn test_main.go

--- go-vs-php-file-parse/lixuancn/test_main.go
+++ go-vs-php-file-parse/lixuancn/test_main.go
@@ -6,7 +6,6 @@ import (
 	"io"
 	"os"
 	"strconv"
-	"strings"
 	//	"runtime"
 	"bytes"
 	"time"
@@ -25,7 +24,7 @@ func main() {
 	endTime := time.Now().UnixNano()
 	fmt.Println(startTime)
 	fmt.Println(endTime)
-	fmt.Println("运行时间:", (endTime-startTime)/1000/1000, "毫秒")
+	fmt.Println("运行时间(optimated):", (endTime-startTime)/1000/1000, "毫秒")
 }
 
 func parse(file string) {
@@ -40,14 +39,12 @@ func parse(file string) {
 	var errList []error
 	//总流量
 	var totalSize uint64
-	//过滤流量
-	var keepStoppSize uint64
 
 	var errMsg error
 	var line []byte
-	var size uint64
-	var sizeTmp int64
 	var lineNum uint64 = 0
+
+	var keepStopBuf = []byte("keep_stopp=on")
 
 	bfRd := bufio.NewReader(f)
 	for {
@@ -56,18 +53,19 @@ func parse(file string) {
 		//判断是否结尾
 		if errMsg == io.EOF {
 			fmt.Println("有效大小:", totalSize, "B")
-			fmt.Println("已过滤大小:", keepStoppSize, "B")
 			return
 		}
-
-		slice := bytes.SplitN(line, []byte(" "), 10)
-		sizeTmp, _ = strconv.ParseInt(string(slice[8]), 10, 0)
-		size = uint64(sizeTmp)
-		if strings.Contains(string(slice[5]), "keep_stopp=on") {
-			keepStoppSize += size
+		if bytes.Contains(line, keepStopBuf) {
 			continue
 		}
-		totalSize += size
+		slice := bytes.SplitN(line, []byte(" "), 10)
+		if len(slice) < 9 {
+			continue
+		}
+		size, _ := strconv.ParseInt(string(slice[8]), 10, 0)
+		// =
+
+		totalSize += uint64(size)
 		if errMsg != nil {
 			errList = append(errList, errMsg)
 			continue
